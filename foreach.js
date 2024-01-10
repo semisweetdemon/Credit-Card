aecc9d3e99f2cb288522535c645016bb7d4fe97c
semisweetdemon <semisweetdemon@gmail.com>
2024-01-10 12:55:27 +0600

    new fix

--- foreach.js
+++ foreach.js
@@ -1,4 +1,8 @@
-let arr = [];
+// sd = (x) => +[...(x + '')].map((n) => n * n).join('');
+// console.log(sd(3214));
 
-arr.forEach((el) => console.log(el));
-arr.map((el) => console.log(el));
+// console.log(2025270009 - 4 * 506295001);
+// console.log(309 - 4 * 37);
+// console.log((45003 - 2 * 22501) * (45003 + 2 * 22501));
+
+console.log(Math.floor(Math.random() * 5 + 1));
